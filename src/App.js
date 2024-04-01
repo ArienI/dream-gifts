024e75f37537553e142bf23e0bc3232fb40aebf4
Arien <Laurilin2022@gmail.com>
2024-04-01 02:35:50 +0200

    meow

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './images/1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>=^._.^=meow=^._.^=</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
+
+// < !DOCTYPE html >
+// <html lang="ru">
+//   <head>
+//     <meta charset="UTF-8">
+//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <meta name="description" content="DreamGifts">
+//           <meta name="keywords" content="gifts, present, holiday, подарки">
+//             <meta name="author" content="Arien">
+//               <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
+//                 <link rel="icon" href="./images/favicon-16x16.png" type="image/png" sizes="16x16">
+//                   <link rel="icon" href="./images/favicon-32x32.png" type="image/png" sizes="32x32">
+//                     <link rel="icon" href="./images/android-chrome-192x192.png" type="image/png" sizes="192x192">
+//                       <link rel="icon" href="./images/android-chrome-512x512.png" type="image/png" sizes="512x512">
+//                         <link rel="apple-touch-icon" href="./images/apple-touch-icon.png">
+//                           <title>Dream Gifts</title>
+//                         </head>
+//                         <body>
+
+//                           <noscript>You need to enable JavaScript to run this app.</noscript>
+//                           <div id="root"></div>
+//                           <div id="modal"></div>
+
+//                         </body>
+//                       </html>
