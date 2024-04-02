3b5bc219845078ed728c202455feb1e209833c88
Arien <Laurilin2022@gmail.com>
2024-04-02 04:30:19 +0200

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Header.module.css';
+import Logo from '../../images/Dream Gifts.png';
+
+
+export default function Header() {
+  return (
+    <header className={styles.header}>
+      <div className={styles.content}>
+        <div className={styles.navigation}>
+          <button className={styles.button}>my presents</button>
+          <button className={styles.button}>other gifts</button>
+        </div>
+        <div className={styles.logo}>
+          <img src={Logo} alt='DreamGifts' className={styles.image} />
+        </div>
+        <div className={styles.profile}>
+          <button className={styles.button}>login</button>
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,27 +18,3 @@ reportWebVitals();
 
 
 
-// < !DOCTYPE html >
-// <html lang="ru">
-//   <head>
-//     <meta charset="UTF-8">
-//       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//         <meta name="description" content="DreamGifts">
-//           <meta name="keywords" content="gifts, present, holiday, подарки">
-//             <meta name="author" content="Arien">
-//               <link rel="icon" href="./images/favicon.ico" type="image/x-icon">
-//                 <link rel="icon" href="./images/favicon-16x16.png" type="image/png" sizes="16x16">
-//                   <link rel="icon" href="./images/favicon-32x32.png" type="image/png" sizes="32x32">
-//                     <link rel="icon" href="./images/android-chrome-192x192.png" type="image/png" sizes="192x192">
-//                       <link rel="icon" href="./images/android-chrome-512x512.png" type="image/png" sizes="512x512">
-//                         <link rel="apple-touch-icon" href="./images/apple-touch-icon.png">
-//                           <title>Dream Gifts</title>
-//                         </head>
-//                         <body>
-
-//                           <noscript>You need to enable JavaScript to run this app.</noscript>
-//                           <div id="root"></div>
-//                           <div id="modal"></div>
-
-//                         </body>
-//                       </html>
